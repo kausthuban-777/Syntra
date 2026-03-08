522e3dad57f447976aa7032d4103df115ff6933c
kausthuban-777 <kausthuban.m@gmail.com>
2026-03-08 20:36:53 +0530

    Project Setup

---
+++ src/hr-crm-webapp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.crm.app',
+  appName: 'hr-crm',
+  webDir: 'dist/hr-crm-webapp/browser'
+};
+
+export default config;
